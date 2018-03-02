e6eff8a342a3b00136f472e735159cf0b855e4d0
karlo <karlocamacho@gmail.com>
2018-03-02 11:24:47 -0800

    Add Server Message

---
+++ Data/SetUserReportEnableOrDisable.cs
@@ -0,0 +1,49 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GTI.Modules.Shared;
+using System.IO;
+
+namespace GTI.Modules.ReportCenter.Data
+{
+
+    class SetUserReportEnableOrDisable : ServerMessage
+    {
+        private List<ReportInfo> mListOfListReportsEnableDisable;
+
+        public SetUserReportEnableOrDisable(List<ReportInfo> ListOfListReportsEnableDisable)
+        {
+            m_id = 18252;
+            mListOfListReportsEnableDisable = ListOfListReportsEnableDisable;
+        }
+
+        protected override void PackRequest()
+        {
+            MemoryStream requestStream = new MemoryStream();            // Create the streams we will be writing to.
+            BinaryWriter requestWriter = new BinaryWriter(requestStream, Encoding.Unicode);
+
+            requestWriter.Write((ushort)mListOfListReportsEnableDisable.Count());
+
+            foreach (ReportInfo rptInfo in mListOfListReportsEnableDisable)  //settings count
+            {
+                requestWriter.Write((int)rptInfo.ID);
+                requestWriter.Write(1);
+                requestWriter.Write((ushort)rptInfo.DisplayName.Length);
+                requestWriter.Write(rptInfo.DisplayName.ToCharArray());
+            }
+
+            m_requestPayload = requestStream.ToArray();
+            requestWriter.Close();            // Close the streams.bn
+        }
+
+        //protected override void UnpackResponse()
+        //{
+        //    base.UnpackResponse();
+
+        //}
+    }
+}
+
+

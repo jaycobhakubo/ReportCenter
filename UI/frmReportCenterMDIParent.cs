70520b9192fa2c9263ae4eb68bd6e48798c2d39d
karlo <karlocamacho@gmail.com>
2018-04-16 11:41:39 -0700

    Issue with security on using the io compression working with 4.5 framework.

--- UI/frmReportCenterMDIParent.cs
+++ UI/frmReportCenterMDIParent.cs
@@ -17,6 +17,7 @@ using GTI.Modules.Shared;
 using GTI.Modules.ReportCenter.Business;
 using GTI.Modules.ReportCenter.Properties;
 using System.IO;
+using System.IO.Compression;
 
 namespace GTI.Modules.ReportCenter.UI
 {
@@ -139,25 +140,41 @@ namespace GTI.Modules.ReportCenter.UI
         private void importFileMenu_Click(object sender, EventArgs e)
         {
             var rptFileSource = new OpenFileDialog();
-            rptFileSource.Title = "Please select a rpt file to import.";
-            rptFileSource.Filter = "Report File |*.rpt";
-            rptFileSource.Multiselect = true;
+            rptFileSource.Title = "Please select a zip file to import.";
+            rptFileSource.Filter = "Report File |*.zip";
+            //rptFileSource.Multiselect = true;
 
             //lets move this file
             if (rptFileSource.ShowDialog() == DialogResult.OK)
             {
-                string[] moveFrom = rptFileSource.FileNames;
-                string moveTo = @"C:\GameTech\Reports\";
-                string copyTo = @"C:\GameTech\Reports\";
+                //Now lets unzip the file
+                string zipPath = @"D:\project_test\project_test.zip";
+                //string extractPath = @"c:\example\extract";
 
-                foreach (string files in moveFrom)
+                using (ZipArchive archive = ZipFile.OpenRead(zipPath))
                 {
-                    var x = Path.GetFileName(files);
-                    var destinationFile = System.IO.Path.Combine(copyTo, x);
-                   // Directory.Move(files, Path.Combine(moveTo, Path.GetFileName(files)));
-                    System.IO.File.Copy(files, destinationFile, true);
-            
-                }            
+                    foreach (ZipArchiveEntry entry in archive.Entries)
+                    {
+                        if (entry.FullName.EndsWith(".txt", StringComparison.OrdinalIgnoreCase))
+                        {
+                            //entry.ExtractToFile(Path.Combine(extractPath, entry.FullName));
+                        }
+                    }
+                } 
+
+
+                //string[] moveFrom = rptFileSource.FileNames;
+                //string moveTo = @"C:\GameTech\Reports\";
+                //string copyTo = @"C:\GameTech\Reports\";
+
+                //foreach (string files in moveFrom)
+                //{
+                //    var x = Path.GetFileName(files);
+                //    var destinationFile = System.IO.Path.Combine(copyTo, x);
+                //    // Directory.Move(files, Path.Combine(moveTo, Path.GetFileName(files)));
+                //    System.IO.File.Copy(files, destinationFile, true);
+
+                //}            
             }
               
         }
